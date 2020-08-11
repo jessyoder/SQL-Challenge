4e8c5e071e5114a133c0f6a73f2ca32a7ac6de58
Jess Yoder <jessica.m.yoder@gmail.com>
2020-08-11 16:35:54 -0400

    fixed employees_create to include primary and foreign keys

--- EmployeesSQL/employees_create.sql
+++ EmployeesSQL/employees_create.sql
@@ -1,20 +1,23 @@
+-- Create tables, primary keys, foreign keys
 CREATE TABLE departments (
-	dept_no VARCHAR,
+	dept_no VARCHAR NOT NULL PRIMARY KEY,
 	dept_name VARCHAR
 );
 
 CREATE TABLE dept_emp (
 	emp_no INT,
-	dept_no VARCHAR
+	dept_no VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR,
-	emp_no INT PRIMARY KEY
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
+	emp_no INT NOT NULL PRIMARY KEY,
 	emp_title VARCHAR,
 	birth_date DATE,
 	first_name VARCHAR,
@@ -24,25 +27,32 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE salaries (
-	emp_no INT PRIMARY KEY,	
-	salary INT
+	emp_no INT,	
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE titles (
-	title_id VARCHAR PRIMARY KEY,
+	title_id VARCHAR NOT NULL PRIMARY KEY,
 	title VARCHAR
 );
 
+ALTER TABLE departments
+ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
+
+ALTER TABLE dept_emp
+ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
+
+ALTER TABLE dept_manager
+ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
+
+ALTER TABLE employees
+ADD FOREIGN KEY (emp_title) REFERENCES titles(title_id);
+
+-- Query all fields from the tables
 SELECT * FROM departments;
 SELECT * FROM dept_emp;
 SELECT * FROM dept_manager;
 SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
-
-DROP TABLE departments;
-DROP TABLE dept_emp;
-DROP TABLE dept_manager;
-DROP TABLE employees;
-DROP TABLE salaries;
-DROP TABLE titles;
--- EmployeesSQL/employees_db.sql
+++ EmployeesSQL/employees_db.sql
@@ -5,8 +5,6 @@ INNER JOIN salaries AS s ON
 e.emp_no=s.emp_no;
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-SELECT * FROM employees;
-
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
