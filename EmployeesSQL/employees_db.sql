f5e4752484950e6d91da5ac138d9048df093de91
Jess Yoder <jessica.m.yoder@gmail.com>
2020-08-11 15:40:06 -0400

    Finished coding employees_db but not bonus

--- EmployeesSQL/employees_db.sql
+++ EmployeesSQL/employees_db.sql
@@ -21,14 +21,46 @@ JOIN employees AS e ON
 m.emp_no=e.emp_no;
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments AS d
+JOIN dept_emp AS m ON
+d.dept_no=m.dept_no
+JOIN employees AS e ON
+m.emp_no=e.emp_no;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments AS d
+JOIN dept_emp AS m ON
+d.dept_no=m.dept_no
+JOIN employees AS e ON
+m.emp_no=e.emp_no
+WHERE dept_name = 'Sales';
 
--- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, 
+-- and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments AS d
+JOIN dept_emp AS m ON
+d.dept_no=m.dept_no
+JOIN employees AS e ON
+m.emp_no=e.emp_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT 
+    last_name, 
+    COUNT(last_name)
+FROM
+    employees
+GROUP BY last_name
+HAVING COUNT(last_name) > 1
+ORDER BY last_name DESC;
 
 -- BONUS
 -- Import the SQL database into Pandas. (See Hints!)
